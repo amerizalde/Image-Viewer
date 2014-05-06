c0153031c1f5e48464956dc9ee9cfb2c1e0d360d
Andrew Merizalde <andrewmerizalde@hotmail.com>
2014-05-06 14:53:53 -0600

    solved channel seperation

--- filters/image_processor.py
+++ filters/image_processor.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import pygame
 import basic_modes as basic
 import advanced_modes as adv
@@ -11,29 +12,30 @@ class Layer(object):
     active = True
 
     apply_filter = {
-        'multiply':     (lambda (self, other): self.filter(basic.multiply, other)),
-        'color_burn':   (lambda (self, other): self.filter(basic.color_burn, other)),
-        'linear_burn':  (lambda (self, other): self.filter(basic.linear_burn, other)),
-        'screen':       (lambda (self, other): self.filter(basic.screen, other)),
-        'color_dodge':  (lambda (self, other): self.filter(basic.color_dodge, other)),
-        'linear_dodge': (lambda (self, other): self.filter(basic.linear_dodge, other)),
-        'subtract':     (lambda (self, other): self.filter(basic.subtract, other)),
-        'divide':       (lambda (self, other): self.filter(basic.divide, other)),
-        'darken':       (lambda (self, other): self.filter(basic.darken, other)),
-        'lighten':      (lambda (self, other): self.filter(basic.lighten, other)),
-        'overlay':      (lambda (self, other): self.filter(adv.overlay, other)),
-        'soft_light':   (lambda (self, other): self.filter(adv.soft_light, other)),
-        'hard_light':   (lambda (self, other): self.filter(adv.hard_light, other)),
-        'vivid_light':  (lambda (self, other): self.filter(adv.vivid_light, other)),
-        'linear_light': (lambda (self, other): self.filter(adv.linear_light, other)),
-        'pin_light':    (lambda (self, other): self.filter(adv.pin_light, other)),
-        'hard_mix':     (lambda (self, other): self.filter(adv.hard_mix, other))
+        'multiply':     (lambda x: x[0].filter(basic.multiply, x[1])),
+        'color_burn':   (lambda x: x[0].filter(basic.color_burn, x[1])),
+        'linear_burn':  (lambda x: x[0].filter(basic.linear_burn, x[1])),
+        'screen':       (lambda x: x[0].filter(basic.screen, x[1])),
+        'color_dodge':  (lambda x: x[0].filter(basic.color_dodge, x[1])),
+        'linear_dodge': (lambda x: x[0].filter(basic.linear_dodge, x[1])),
+        'subtract':     (lambda x: x[0].filter(basic.subtract, x[1])),
+        'divide':       (lambda x: x[0].filter(basic.divide, x[1])),
+        'darken':       (lambda x: x[0].filter(basic.darken, x[1])),
+        'lighten':      (lambda x: x[0].filter(basic.lighten, x[1])),
+        'overlay':      (lambda x: x[0].filter(adv.overlay, x[1])),
+        'soft_light':   (lambda x: x[0].filter(adv.soft_light, x[1])),
+        'hard_light':   (lambda x: x[0].filter(adv.hard_light, x[1])),
+        'vivid_light':  (lambda x: x[0].filter(adv.vivid_light, x[1])),
+        'linear_light': (lambda x: x[0].filter(adv.linear_light, x[1])),
+        'pin_light':    (lambda x: x[0].filter(adv.pin_light, x[1])),
+        'hard_mix':     (lambda x: x[0].filter(adv.hard_mix, x[1]))
     }
 
     def __init__(self, filename):
         try:
-            self.pixels = pygame.surfarray.array3d(
+            self._pixels = pygame.surfarray.array3d(
                 pygame.image.load(filename))
+            self.pixels = self._pixels
         except pygame.error, message:
             print "Cannot load image:", filename
             raise SystemExit, message
@@ -43,24 +45,28 @@ class Layer(object):
         blend_mode to every channel of the pixel of self.
         """
         for i in self.pixels:
+            pprint(i)
             for j in other_image.pixels:
-                i_r, i_g, i_b = i[:, :, 0], i[:, :, 1], i[:, :, 2]
-                j_r, j_g, j_b = j[:, :, 0], j[:, :, 1], j[:, :, 2]
+                i_r, i_g, i_b = i[0], i[1], i[2]
+                j_r, j_g, j_b = j[0], j[1], j[2]
 
                 i_r = func(i_r, j_r)
                 i_g = func(i_g, j_g)
                 i_b = func(i_b, j_b)
-                i[:, :, 0], i[:, :, 1], i[:, :, 2] = i_r, i_g, i_b
+                i[0], i[1], i[2] = i_r, i_g, i_b
 
     def saveLayer(self, filename):
         try:
             surf = pygame.surfarray.make_surface(self.pixels)
-        except IndexError, message:
+        except IndexError:
             (width, height, colors) = self.pixels.shape
             surf = pygame.display.set_mode((width, height))
             pygame.surfarray.blit_array(surf, self.pixels)
 
         pygame.image.save(surf, filename)
+
+    def reset(self):
+        self.pixels = self._pixels
 
 
 class Stack(object):
@@ -84,7 +90,11 @@ class Stack(object):
         for layer in self.layers:
             if layer.blend_mode != 'normal':
                 # assuming index 0 is always the background layer and 'normal'...
-                layer.apply_filter[layer.blend_mode](self.layers[self.layers.index(layer) - 1])
+                layer.apply_filter[layer.blend_mode]((
+                                    # passing the layer, and the previous layer
+                                    layer,
+                                    self.layers[self.layers.index(layer) - 1]))
+        # save the top layer
         self.layers[-1].saveLayer(filename)
 
 
@@ -112,27 +122,20 @@ def saveSurface(pixels, filename):
 
 
 if __name__ == "__main__":
-    pixels = getPixelArray("gollum.jpg")
-    print type(pixels)
+    
+    a = Layer('gollum.jpg')
+    red_channel = Layer('gollum.jpg')
+    green_channel = Layer('gollum.jpg')
+    blue_channel = Layer('gollum.jpg')
+    x, y, z = a.pixels.shape
+    for i in xrange(x):
+        for j in xrange(y):
+            ii = i
+            jj = j
+            red_channel.pixels[ii, jj] = a.pixels[ii, jj] * [1, 0, 0]
+            green_channel.pixels[ii, jj] = a.pixels[ii, jj] * [0, 1, 0]
+            blue_channel.pixels[ii, jj] = a.pixels[ii, jj] * [0, 0, 1]
 
-    curr_pixels = pixels
-    curr_pixels[100] = 0
-    saveSurface(curr_pixels, "gollum_black_column.jpg")
-
-    # [from_left:from_right, from_top:from_bottom, red:green:blue]
-    curr_pixels = pixels[50:-50, 200:, :]
-    saveSurface(curr_pixels, 'gollum_crop.jpg')
-
-    curr_pixels = pixels[::2, ::2, :]
-    saveSurface(curr_pixels, 'gollum_scaled.jpg')
-
-    curr_pixels = pixels[::-1, ::-1, :]
-    saveSurface(curr_pixels, 'gollum_flip.jpg')
-
-    stack = Stack()
-    layer_1 = Layer('gollum.jpg')
-    layer_2 = Layer('gollum.jpg')
-    layer_2.blend_mode = 'multiply'
-    stack.add_new_layer(layer_1)
-    stack.add_new_layer(layer_2)
-    stack.save('gollum_multiply.jpg')
+    red_channel.saveLayer('red_channel.jpg')
+    green_channel.saveLayer('green_channel.jpg')
+    blue_channel.saveLayer('blue_channel.jpg')
